64fd0745ef3e2f76a7523346c49ea17d857f523b
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-04-16 11:19:51 +0900

    5 v-on:click

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-var app3 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [{
-        text: 'Learn JavaScript'
-      },
-      {
-        text: 'Learn Vue'
-      },
-      {
-        text: 'Build something awesome'
-      }
-    ]
+var app5 = new Vue({
+  el: '#app-5',
+  data: { message: 'Hello Vue.js!' },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
