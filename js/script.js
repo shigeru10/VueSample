3f0b8831a781f1e0145c8837de08b29b4d90d8f4
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-04-16 11:42:08 +0900

    7 component

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-var app6 = new Vue({
-  el: '#app-6',
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
   data: {
-    message: 'Hello Vue!'
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
   }
 })
